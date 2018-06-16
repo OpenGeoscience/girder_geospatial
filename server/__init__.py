33860a7f79c19648a59646b481d01dd90106f78d
Doruk Ozturk <doruk.ozturk@kitware.com>
2018-06-15 21:29:30 -0400

    Add a new endpoint to create geospatial metadata on items

--- server/__init__.py
+++ server/__init__.py
@@ -1,46 +1,21 @@
-import pkg_resources
 from girder import events
 from girder.models.file import File
 from girder.models.item import Item
-from girder.models.assetstore import Assetstore
-from girder.utility import assetstore_utilities
-from girder.utility._cache import cache
-from geometa.schema import BaseSchema
-from geometa import GEOSPATIAL_FIELD
-from geometa import CannotHandleError
-from .rest import geometa_handler
+from .rest import geometa_search_handler, geometa_create_handler
+from .rest import create_geometa
 
 
-def _get_girder_path(girder_file):
-    assetstore = Assetstore().load(girder_file['assetstoreId'])
-    adapter = assetstore_utilities.getAssetstoreAdapter(assetstore)
-    return adapter.fullPath(girder_file)
-
-
-@cache.cache_on_arguments()
-def get_handlers():
-    entry_points = pkg_resources.iter_entry_points('geometa.formats')
-    return {e.name: e.load() for e in entry_points}
-
-
-def upload_handler(event):
+def file_upload_handler(event):
     _id = event.info['file']['_id']
     girder_file = File().load(_id, force=True)
     girder_item = Item().load(event.info['file']['itemId'], force=True)
-    path = _get_girder_path(girder_file)
-    for entry_point_name, entry_point in get_handlers().items():
-        try:
-            metadata = entry_point(path)
-            schema = BaseSchema()
-            schema.load(metadata)
-            girder_item['geometa'] = metadata
-            Item().save(girder_item)
-            Item().collection.create_index([(GEOSPATIAL_FIELD, "2dsphere")])
-        except CannotHandleError:
-            pass
+    create_geometa(girder_item, girder_file)
 
 
 def load(info):
     events.bind('model.file.finalizeUpload.after',
-                info['name'], upload_handler)
-    info['apiRoot'].item.route('GET', ('geometa',), geometa_handler)
+                info['name'], file_upload_handler)
+    info['apiRoot'].item.route('GET', ('geometa',),
+                               geometa_search_handler)
+    info['apiRoot'].item.route('PUT', (':id', 'geometa'),
+                               geometa_create_handler)
